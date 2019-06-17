ae10835da5d39ef80107d970196e8e1faaa1dd1f
gitcart <gitcart1011@gmail.com>
2019-06-17 12:58:42 -0500

    java

---
+++ database/mascota.sql
@@ -0,0 +1,38 @@
+/*
+SQLyog Community v8.71 
+MySQL - 5.6.26 : Database - mascota
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`mascota` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `mascota`;
+
+/*Table structure for table `mascota` */
+
+DROP TABLE IF EXISTS `mascota`;
+
+CREATE TABLE `mascota` (
+  `idMascota` int(11) NOT NULL,
+  `nombreMascota` varchar(45) NOT NULL,
+  `nombreCliente` varchar(45) NOT NULL,
+  `raza` varchar(45) NOT NULL,
+  PRIMARY KEY (`idMascota`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `mascota` */
+
+insert  into `mascota`(`idMascota`,`nombreMascota`,`nombreCliente`,`raza`) values (1,'Juancito','Fidel cartolin','chiguagua'),(2,'mascos','mas','mqsa'),(3,'pedrito','fidel','chiwawa'),(4,'puma','MARTIN','piybull'),(5,'pumita','martincito','martir'),(6,'puma','cartolin','pitbull');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
